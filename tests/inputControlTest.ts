97e4e49f2306147e62ee99dbda2246d2d82e98af
charles <cordincpublic@gmail.com>
2023-08-21 20:10:54 +0800

    github workflows

--- tests/inputControlTest.ts
+++ tests/inputControlTest.ts
@@ -1,4 +1,4 @@
-import { PressInputControl } from "../src/kassite/RtsCamera";
+import { PressInputControl } from "../src/kassite/InputControl";
 
 test('should return true', () => {
     expect(new PressInputControl().editable).toBe(true);
