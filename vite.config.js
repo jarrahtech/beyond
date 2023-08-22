3cffd78186f1bbf1df84b95424590d6cfe3e6fad
charles <cordincpublic@gmail.com>
2023-08-22 11:35:36 +0800

    deploy to pages action

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+var baseUrl = process.env['BASE_URL'];
+if (baseUrl == undefined || baseUrl.trim()==="/") { // required as Electron.js does not like 
+  console.log("Rewriting root Base URL to empty")
+  baseUrl = "";
+  process.env['BASE_URL'] = baseUrl;
+} else {
+  baseUrl = baseUrl.trim();
+}
+console.log("Base URL: '"+baseUrl+"'")
+export default defineConfig({
+  base: baseUrl,
+});
+
