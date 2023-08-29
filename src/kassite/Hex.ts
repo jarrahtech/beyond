7e828125ac7bd73e02d0e33a230b3401923d9638
Charles Cordingley <cordincpublic@gmail.com>
2023-08-29 11:23:22 +0800

    dev test

--- src/kassite/Hex.ts
+++ src/kassite/Hex.ts
@@ -9,3 +9,7 @@ export const  circleAngle = hexSides*sideAngle;
 
 export const flatTopHexPixelPoints = [new BABYLON.Vector2(0, root3/4.0), new BABYLON.Vector2(0.25, root3/2.0), new BABYLON.Vector2(0.75, root3/2.0), new BABYLON.Vector2(1, root3/4.0), new BABYLON.Vector2(0.75, 0), new BABYLON.Vector2(0.25, 0)];
 export const pointyTopHexPixelPoints = [new BABYLON.Vector2(0, 0.25), new BABYLON.Vector2(0, 0.75), new BABYLON.Vector2(root3/4.0, 1), new BABYLON.Vector2(root3/2.0, 0.75), new BABYLON.Vector2(root3/2.0, 0.25), new BABYLON.Vector2(root3/4.0, 0)];
+
+export interface HexCoordSystem {
+  
+}
