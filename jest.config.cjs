dddd836581db7656840a6e92124ae514775584df
charles <cordincpublic@gmail.com>
2023-11-03 15:50:02 +0800

    fix jest not handling babylonjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,19 @@ module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
   testRegex: 'tests/.*\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  //SLOW - transform javascript
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  //SLOW - transform node modules
+  transformIgnorePatterns: [],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true, // to make type check faster
+      tsConfig: {            // to have tsc transform .js files
+        allowJs: true,
+        checkJs: false,
+      },
+    }
+  },
 };
